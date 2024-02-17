2821c505d32db610d555ea1f92d2b55f6ee8da68
Honziiis19 <jan.rainer@gym-tisnov.cz>
2024-02-17 18:38:40 +0100

    Správnej create

--- sql/Projekt_create.sql
+++ sql/Projekt_create.sql
@@ -1,5 +1,5 @@
 -- MySQL Workbench Forward Engineering
-
+DROP DATABASE `infotabdb`;
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
@@ -18,7 +18,7 @@ USE `infotabdb` ;
 -- Table `mydb`.`uzivatele`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `infotabdb`.`uzivatele` (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
   `jmeno` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL UNIQUE,
   `heslo` VARCHAR(45) NOT NULL,
@@ -29,6 +29,8 @@ ENGINE = InnoDB;
 CREATE INDEX idx_jmeno
 ON uzivatele(jmeno);
 
+INSERT INTO uzivatele(jmeno, email, heslo, admin)
+VALUES('admin', 'admin@email.com', 'heslo123', '1');
 -- -----------------------------------------------------
 -- Table `mydb`.`prezentace`
 -- -----------------------------------------------------
@@ -36,21 +38,24 @@ CREATE TABLE IF NOT EXISTS `infotabdb`.`prezentace` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
   `nadpis` VARCHAR(100) NULL,
   `obsah` VARCHAR(1000) NULL,
-  `scroll text` VARCHAR(1000) NULL,
+  `scrolltext` VARCHAR(1000) NULL,
   `vyditelnost` DATETIME NULL,
-  `created.by` VARCHAR(50),
-  `modified.by` VARCHAR(50),
+  `createdby` VARCHAR(50),
+  `modifiedby` VARCHAR(50),
   `aktivni` ENUM("0", "1"),
+  uzivatele_id INT,
   FOREIGN KEY (uzivatele_id) REFERENCES uzivatele(id)
   )
 ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `infotab`.`snimky` (
+CREATE TABLE IF NOT EXISTS `infotabdb`.`snimky` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+  prezentace_id INT,
   FOREIGN KEY (prezentace_id) REFERENCES prezentace(id),
-  `htmltext` VARCHAR(5000)
+  `htmltext` TEXT(65535)
   )
 ENGINE = InnoDB;
+
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
